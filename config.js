e9b8eb35f4b501e4c00e5ac3515a9d68d094395d
Oligh237 <106449105+Oligh237@users.noreply.github.com>
2023-07-01 16:50:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ global.lApiKey = {// if this API key get expired then please go to https://api.l
 
 
 global.botname = "Queen Anya" 
-global.author = "Enter Author Name Here"  
+global.author = "OLIBOT"  
 global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
 global.myweb = "Enter Your Any Website Profile Link Here"
 global.footer = "©\t" + "Enter Your Watermark Here"
@@ -33,13 +33,13 @@ global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
 global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
 
 
-global.ownername = "Enter Owner Name Here"    
+global.ownername = "OLIBOT"    
 global.ownernumber = ["237657255084"]
 global.ytname = "YT: Enter Your Channel Name Here"
 global.continent = "Africa" // your continent name 
 global.region = "Cameroon"
 global.state = "Ngaoundere" // your state name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
+global.timezone = "aAfrica/Cameroon" // search on google if you don't know the timezone of your country //
 global.instagramId = "Enter Your Insta ID Here"
 global.email = "cedrick.monthe@gmail.com"
 
